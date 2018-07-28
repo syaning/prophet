1b5f1d7c0a9604ee9015d7a314de852cdc59e40c
syaning <syaningv@gmail.com>
2018-07-28 10:13:24 +0800

    fullscreen support

---
+++ src/components/layout/navbar/FullScreen.vue
@@ -0,0 +1,33 @@
+<template>
+  <Icon class="full-screen-icon" :type="fullScreen ? 'arrow-shrink' : 'arrow-expand'" @click="toggleFullScreen"></Icon>
+</template>
+
+<script>
+import screenfull from 'screenfull'
+
+export default {
+  data() {
+    return {
+      fullScreen: false
+    }
+  },
+
+  methods: {
+    toggleFullScreen() {
+      this.fullScreen = !this.fullScreen
+      screenfull.toggle()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.full-screen-icon {
+  cursor: pointer;
+  color: #80848f;
+
+  &:hover {
+    color: #2d8cf0;
+  }
+}
+</style>
--- src/components/layout/navbar/index.vue
+++ src/components/layout/navbar/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div style="float: right;">
+    <full-screen style="margin-right: 1rem;"></full-screen>
     <lang-select></lang-select>
   </div>
 </template>
 
 <script>
+import FullScreen from './FullScreen'
 import LangSelect from './LangSelect'
 
 export default {
-  components: { LangSelect }
+  components: { FullScreen, LangSelect }
 }
 </script>
