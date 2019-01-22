b6b324c2d5e62171e3ec1b76b64275bed2ce064d
syaning <syaningv@gmail.com>
2019-01-23 00:09:53 +0800

    feat: advanced form

--- src/views/form/advanced-form/index.vue
+++ src/views/form/advanced-form/index.vue
@@ -15,10 +15,13 @@
           </Col>
           <Col :xs="24" :sm="12" :md="{ span: 6, offset: 2}">
             <FormItem label="仓库域名">
-              <Input v-model="warehouse.domain" />
+              <Input v-model="warehouse.domain" placeholder="请输入">
+                <span slot="prepend">https://</span>
+                <span slot="append">.com</span>
+              </Input>
             </FormItem>
           </Col>
-          <Col :sm="24" :md="{ span: 6, offset: 2}">
+          <Col :sm="24" :md="{ span: 8, offset: 2}">
             <FormItem label="仓库管理员">
               <Select v-model="warehouse.owner" placeholder="请选择管理员">
                 <Option v-for="user in users" :key="user.id" :value="user.id">
@@ -28,7 +31,87 @@
             </FormItem>
           </Col>
         </Row>
+
+        <Row :gutter="16">
+          <Col :xs="24" :sm="12" :md="6">
+            <FormItem label="审批人">
+              <Select v-model="warehouse.approver" placeholder="请选择审批人">
+                <Option v-for="user in users" :key="user.id" :value="user.id">
+                  {{ user.name }}
+                </Option>
+              </Select>
+            </FormItem>
+          </Col>
+          <Col :xs="24" :sm="12" :md="{ span: 6, offset: 2}">
+            <FormItem label="生效日期">
+              <DatePicker v-model="warehouse.dateRange" type="daterange" placeholder="开始日期 ~ 结束日期" style="width: 100%;"></DatePicker>
+            </FormItem>
+          </Col>
+          <Col :xs="24" :sm="12" :md="{ span: 8, offset: 2}">
+            <FormItem label="仓库类型">
+              <Select v-model="warehouse.type" placeholder="请选择仓库类型">
+                <Option v-for="type in types" :key="type.value" :value="type.value">
+                  {{ type.text }}
+                </Option>
+              </Select>
+            </FormItem>
+          </Col>
+        </Row>
       </Form>
+    </Card>
+
+    <Card dis-hover :bordered="false" title="任务管理" style="margin-top: 20px;">
+      <Form :model="task">
+        <Row :gutter="16">
+          <Col :xs="24" :sm="12" :md="6">
+            <FormItem label="任务名">
+              <Input v-model="task.name" placeholder="请输入任务名" />
+            </FormItem>
+          </Col>
+          <Col :xs="24" :sm="12" :md="{ span: 6, offset: 2}">
+            <FormItem label="任务描述">
+              <Input v-model="task.name" placeholder="请输入任务描述" />
+            </FormItem>
+          </Col>
+          <Col :xs="24" :sm="12" :md="{ span: 8, offset: 2}">
+            <FormItem label="执行人">
+              <Select v-model="task.executor" placeholder="请选择执行人">
+                <Option v-for="user in users" :key="user.id" :value="user.id">
+                  {{ user.name }}
+                </Option>
+              </Select>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row :gutter="16">
+          <Col :xs="24" :sm="12" :md="6">
+            <FormItem label="责任人">
+              <Select v-model="task.owner" placeholder="请选择责任人">
+                <Option v-for="user in users" :key="user.id" :value="user.id">
+                  {{ user.name }}
+                </Option>
+              </Select>
+            </FormItem>
+          </Col>
+          <Col :xs="24" :sm="12" :md="{ span: 6, offset: 2}">
+            <FormItem label="生效时间">
+              <DatePicker v-model="task.time" type="datetime" placeholder="提醒时间" style="width: 100%;"></DatePicker>
+            </FormItem>
+          </Col>
+          <Col :xs="24" :sm="12" :md="{ span: 8, offset: 2}">
+            <FormItem label="任务类型">
+              <Select v-model="task.type" placeholder="请选择任务类型">
+                <Option v-for="type in types" :key="type.value" :value="type.value">
+                  {{ type.text }}
+                </Option>
+              </Select>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    </Card>
+
+    <Card dis-hover :bordered="false" title="成员管理" style="margin-top: 20px;">
     </Card>
   </div>
 </template>
@@ -47,6 +130,14 @@ export default {
         owner: '',
         approver: '',
         dateRange: ['', ''],
+        type: ''
+      },
+      task: {
+        name: '',
+        desc: '',
+        executor: '',
+        owner: '',
+        time: '',
         type: ''
       },
       users: [
