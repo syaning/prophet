93662c82cbf9686fbc0f03d4f5d0455f8ff84056
syaning <syaningv@gmail.com>
2019-02-17 23:28:29 +0800

    update account basic settings

--- src/views/account/settings/basic.vue
+++ src/views/account/settings/basic.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>
-    basic settings
+  <div :class="prefixCls">
+    <div :class="infoClasses">info</div>
+
+    <div :class="avatarClasses">avatar</div>
   </div>
 </template>
+
+<script>
+const prefixCls = 'prophet-account-basic'
+
+export default {
+  data() {
+    return {
+      prefixCls,
+      user: {
+        email: '',
+        nickname: '',
+        description: '',
+        country: '',
+        city: '',
+        address: '',
+        phone: ''
+      }
+    }
+  },
+
+  computed: {
+    infoClasses() {
+      return `${prefixCls}-info`
+    },
+
+    avatarClasses() {
+      return `${prefixCls}-avatar`
+    }
+  }
+}
+</script>
