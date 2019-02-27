c8d8b985dab1abf3886d91bab91296a3230d4e59
syaning <syaningv@gmail.com>
2019-02-27 19:46:49 +0800

    mock passthrough

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,1 +1,8 @@
-require('./modules/user')
+/* eslint-disable no-unused-vars */
+
+import mock from './mock'
+import user from './modules/user'
+
+/* eslint-enable no-unused-vars */
+
+mock.onAny().passThrough()
